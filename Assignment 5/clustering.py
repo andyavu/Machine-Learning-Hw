94219b143705cb4c571fb63b0747b2a207c4cc11
Andy Vu <55299423+andyavu@users.noreply.github.com>
2021-05-08 08:30:26 -0700

    wrong repo

--- Assignment 5/clustering.py
+++ Assignment 5/clustering.py
@@ -1,12 +1,13 @@
 #-------------------------------------------------------------------------
-# AUTHOR: your name
-# FILENAME: title of the source file
-# SPECIFICATION: description of the program
-# FOR: CS 4200- Assignment #5
-# TIME SPENT: how long it took you to complete the assignment
-#-----------------------------------------------------------*/
+# AUTHOR:           Andy Vu
+# FILENAME:         clustering.py
+# SPECIFICATION:    Run k-means multiple times and check which k value
+#                   maximizes the Silhouette coefficient
+# FOR:              CS 4210 - Assignment #5
+# TIME SPENT:       05/04/2021-05/08/2021
+#-------------------------------------------------------------------------
 
-#importing some Python libraries
+# importing some Python libraries
 from sklearn.cluster import KMeans
 from sklearn.cluster import AgglomerativeClustering
 import numpy as np
@@ -15,38 +16,53 @@ import matplotlib.pyplot as plt
 from sklearn.metrics import silhouette_score
 from sklearn import metrics
 
-df = pd.read_csv('training_data.csv', sep=',', header=None) #reading the data by using Pandas library
+df = pd.read_csv('training_data.csv', sep=',', header=None) # reading the data by using Pandas library
 
-#assign your training data to X_training feature matrix
+# assign your training data to X_training feature matrix
+X_training = df.copy()
 
-#run kmeans testing different k values from 2 until 20 clusters
-     #Use:  kmeans = KMeans(n_clusters=k, random_state=0)
-     #      kmeans.fit(X_training)
-     #--> add your Python code
+best_k = 0
+max_score = 0
+s_score = []
+best_kmeans = None
 
+# run kmeans testing different k values from 2 until 20 clusters
+# Use: kmeans = KMeans(n_clusters=k, random_state=0)
+#      kmeans.fit(X_training)
+for k in range(2, 21):
+    kmeans = KMeans(n_clusters=k, random_state=0)
+    kmeans.fit(X_training)
+    # for each k, calculate the silhouette_coefficient by using: silhouette_score(X_training, kmeans.labels_)
+    # find which k maximizes the silhouette_coefficient
+    score = silhouette_score(X_training, kmeans.labels_)
+    s_score.append(score)
+    if score > max_score:
+        best_k = k
+        max_score = score
+        best_kmeans = kmeans
 
-     #for each k, calculate the silhouette_coefficient by using: silhouette_score(X_training, kmeans.labels_)
-     #find which k maximizes the silhouette_coefficient
-     #--> add your Python code here
+# plot the value of the silhouette_coefficient for each k value of kmeans so that we can see the best k
+k = range(2, 21)
+plt.plot(k, s_score, "-o")
+plt.show()
 
-#plot the value of the silhouette_coefficient for each k value of kmeans so that we can see the best k
-#--> add your Python code here
+# reading the validation data (clusters) by using Pandas library
+y_test = pd.read_csv("testing_data.csv", sep=",", header=None)
 
-#reading the validation data (clusters) by using Pandas library
-#--> add your Python code here
-
-#assign your data labels to vector labels (you might need to reshape the row vector to a column vector)
+# assign your data labels to vector labels (you might need to reshape the row vector to a column vector)
 # do this: np.array(df.values).reshape(1,<number of samples>)[0]
-#--> add your Python code here
+labels = np.array(y_test.values).reshape(1, len(y_test))[0]
 
-#Calculate and print the Homogeneity of this kmeans clustering
-print("K-Means Homogeneity Score = " + metrics.homogeneity_score(labels, kmeans.labels_).__str__())
-#--> add your Python code here
+# Calculate and print the Homogeneity of this kmeans clustering
+print("K-Means Homogeneity Score = " + metrics.homogeneity_score(labels, best_kmeans.labels_).__str__())
 
-#rung agglomerative clustering now by using the best value o k calculated before by kmeans
-#Do it:
-#agg = AgglomerativeClustering(n_clusters=<best k value>, linkage='ward')
-#agg.fit(X_training)
 
-#Calculate and print the Homogeneity of this agglomerative clustering
+# rung agglomerative clustering now by using the best value o k calculated before by kmeans
+# Do it:
+# agg = AgglomerativeClustering(n_clusters=<best k value>, linkage='ward')
+# agg.fit(X_training)
+agg = AgglomerativeClustering(n_clusters=best_k, linkage="ward")
+agg.fit(X_training)
+
+# Calculate and print the Homogeneity of this agglomerative clustering
 print("Agglomerative Clustering Homogeneity Score = " + metrics.homogeneity_score(labels, agg.labels_).__str__())
--- Assignment 5/collaborative_filtering.py
+++ Assignment 5/collaborative_filtering.py
@@ -1,31 +1,54 @@
 #-------------------------------------------------------------------------
-# AUTHOR: your name
-# FILENAME: title of the source file
-# SPECIFICATION: description of the program
-# FOR: CS 4200- Assignment #5
-# TIME SPENT: how long it took you to complete the assignment
-#-----------------------------------------------------------*/
+# AUTHOR:           Andy Vu
+# FILENAME:         collaborative_filtering.py
+# SPECIFICATION:    Read file to make user-based recommendations
+# FOR:              CS 4210 - Assignment #5
+# TIME SPENT:       05/04/2021-05/08/2021
+#-------------------------------------------------------------------------
 
-#importing some Python libraries
+# importing some Python libraries
 import numpy as np
 import pandas as pd
 from sklearn.metrics.pairwise import cosine_similarity
 
 df = pd.read_csv('trip_advisor_data.csv', sep=',', header=0) #reading the data by using the Pandas library ()
 
-#iterate over the other 99 users to calculate their similarity with the active user (user 100) according to their category ratings (user-item approach)
-   # do this to calculate the similarity:
-   #vec1 = np.array([[1,1,0,1,1]])
-   #vec2 = np.array([[0,1,0,1,1]])
-   #cosine_similarity(vec1, vec2)
-   #do not forget to discard the first column (User ID) when calculating the similarities
-   #--> add your Python code here
+# iterate over the other 99 users to calculate their similarity with the active user (user 100) according to their category ratings (user-item approach)
+# do this to calculate the similarity:
+# vec1 = np.array([[1,1,0,1,1]])
+# vec2 = np.array([[0,1,0,1,1]])
+# cosine_similarity(vec1, vec2)
+# do not forget to discard the first column (User ID) when calculating the similarities
+columns = list(df.columns)
+columns.pop(0)
+columns.pop(columns.index("galleries"))
+columns.pop(columns.index("restaurants"))
+cos_sim_list = []
+for i in range(99):
+    vec1 = np.array([df[columns].loc[i, :]])
+    vec2 = np.array([df[columns].loc[99, :]])
+    cos_sim = cosine_similarity(vec1, vec2)
+    cos_sim_list.append((cos_sim, df.loc[i, :]))
 
-   #find the top 10 similar users to the active user according to the similarity calculated before
-   #--> add your Python code here
+# find the top 10 similar users to the active user according to the similarity calculated before
+cos_sim_list.sort(key=lambda x: -x[0])
+top_users = cos_sim_list[:10]
 
-   #Compute a prediction from a weighted combination of selected neighbors’ for both categories evaluated (galleries and restaurants)
-   #--> add your Python code here
+sim_sum = 0
+sum_galleries = 0
+sum_restaurants = 0
+# Compute a prediction from a weighted combination of selected neighbors’ for both categories evaluated (galleries and restaurants)
+for sim, user in top_users:
+    # sim = sim[0][0]
+    # sim_sum += sim
+    sim_sum += sim[0][0]
+    temp = [float(x) for x in user[1:]]
+    sum_galleries += (sim[0][0] * (float(user["galleries"]) - sum(temp)/len(temp)))
+    sum_restaurants += (sim[0][0] * (float(user["restaurants"]) - sum(temp)/len(temp)))
 
+user_avg = sum(df[columns].loc[99, :])/len(df[columns].loc[99, :])
+gallery = round(user_avg + (sum_galleries / sim_sum))
+restaurant = round(user_avg + (sum_restaurants / sim_sum))
 
-
+print("Gallery:", gallery)
+print("Restaurant:", restaurant)
